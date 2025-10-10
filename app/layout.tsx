7d6ec76a358ff2a6143dcc0f6974a178beb4f62e
muath alzoubi <moathaza@gmail.com>
2025-10-10 14:46:01 +0000

    asdasd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Bio Links App",
@@ -15,10 +14,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
-      </body>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864"
+          strategy="afterInteractive"
+        />
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-10860808864');
+          `}
+        </Script>
+      </head>
+      <body>{children}</body>
     </html>
   );
 }
