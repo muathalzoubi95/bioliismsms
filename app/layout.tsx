2e12eb96b3ee99e58ce7d7fa8fdd9ec6b56f16cd
Muath K Alzoubi <moathaza@gmail.com>
2025-10-26 04:33:32 +0300

    Change Script tags to script tags in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,22 +18,22 @@ export default function RootLayout({
   return (
     <html lang="en">
           <head>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864" strategy="afterInteractive" />
-        <Script id="google-ads-init" strategy="afterInteractive">
+        <script src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864" strategy="afterInteractive" />
+        <script id="google-ads-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-10860808864');
           `}
-        </Script>
-             <Script id="google-tag-manager" strategy="afterInteractive">
+        </script>
+             <script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NT9TGGQK');`}
-        </Script>
+        </script>
       </head>
       <body
       >
