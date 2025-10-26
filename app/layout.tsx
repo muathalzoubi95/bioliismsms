eb57b9fc9291418965745b4a2d415f921a4e992f
Muath K Alzoubi <moathaza@gmail.com>
2025-10-26 04:37:55 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,50 @@
 import type React from "react"
-import type { Metadata } from "next";
+import type { Metadata } from "next"
 import Script from "next/script"
-import "./globals.css";
-
-
+import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Bio Links App",
   description: "Bio Links app",
-};
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-          <head>
-        <script src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864" strategy="afterInteractive" />
-        <script id="google-ads-init" strategy="afterInteractive">
+      <body>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864" strategy="afterInteractive" />
+        <Script id="google-ads-init" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'AW-10860808864');
           `}
-        </script>
-             <script id="google-tag-manager" strategy="afterInteractive">
+        </Script>
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-NT9TGGQK');`}
-        </script>
-      </head>
-      <body
-      >
-        <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NT9TGGQK"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+        </Script>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NT9TGGQK"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
-  );
+  )
 }
