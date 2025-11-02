e38bed88ae2a90819526cff6b4de6e869a63fd2d
Muath K Alzoubi <moathaza@gmail.com>
2025-11-02 09:33:38 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,34 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+         <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-10860808864');
+            `,
+          }}
+        />
+      </head>
       <body>
-        <Script src="https://www.googletagmanager.com/gtag/js?id=AW-10860808864" strategy="afterInteractive" />
-        <Script id="google-ads-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'AW-10860808864');
-          `}
-        </Script>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NT9TGGQK');`}
-        </Script>
-
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NT9TGGQK"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         {children}
       </body>
     </html>
